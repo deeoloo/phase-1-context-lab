5b98548cabc51087f749e02d596c611f066be5fb
deenoloo <doreenoloo2@gmail.com>
2025-03-22 06:52:28 -0400

    context-lab

--- index.js
+++ index.js
@@ -1,4 +1,65 @@
 /* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRecord){
+    return employeeRecord.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(event => event.date === date).hour;
+    const timeOut = this.timeOutEvents.find(event => event.date === date).hour;
+    return (timeOut - timeIn) / 100;
+
+}
+
+function wagesEarnedOnDate(dateStamp){
+    const hoursWorked = hoursWorkedOnDate.call(this, dateStamp);
+    return hoursWorked*this.payPerHour
+     
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+
+function calculatePayroll(employeeRecords){
+    return employeeRecords.reduce((total, employee)=>{
+      return total + allWagesFor.call(employee)
+    }, 0);
+
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
